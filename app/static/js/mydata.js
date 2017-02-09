335464ca1f805412bfd28aebb442ee70dba6de44
flying <fly0733@gmail.com>
2017-02-09 08:45:30 +0800

    first commit

--- app/static/js/mydata.js
+++
@@ -1,8 +0,0 @@
-$(function(){
-    $('table').tablesorter({
-        widgets        : ['zebra', 'columns'],
-        usNumberFormat : false,
-        sortReset      : true,
-        sortRestart    : true
-    });
-});
