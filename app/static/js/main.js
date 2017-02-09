0c180d294bf36f7584fc28e7c20630ada3fa6f70
flying <fly0733@gmail.com>
2017-02-09 22:49:24 +0800

    12

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -1,3 +1,24 @@
 $(function() {
+    var fontSize = parseFloat(document.documentElement.style.fontSize);
+    var indexdom = $(".indexmenu");
+    if (indexdom.length > 0) {
+        var listTop = $(window).height() - fontSize * 4;
+        var backTop = $(window).height() - fontSize * 2.8;
+        var activeListTop = fontSize * 2.2;
+        indexdom.find(".list").css({ "top": listTop });
+        indexdom.find(".black").css({ "top": backTop });
+
+        indexdom.on("click", ".up", function() {
+            indexdom.toggleClass("active");
+            indexdom.find(".black").css({ "top": 0 });
+            indexdom.find(".list").css({ "top": activeListTop });
+        });
+        indexdom.on("click", ".down", function() {
+            indexdom.toggleClass("active");
+            indexdom.find(".black").css({ "top": backTop });
+            indexdom.find(".list").css({ "top": listTop });
+        });
+
+    };
 
 })
