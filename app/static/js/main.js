518219ff83b9dda6e0252c79840bdcfd2021f9d7
Ricardo <linuxmei@foxmail.com>
2017-02-10 15:09:08 +0800

    12

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -2,11 +2,9 @@ $(function() {
     var fontSize = parseFloat(document.documentElement.style.fontSize);
     var indexdom = $(".indexmenu");
     if (indexdom.length > 0) {
+        $("body >.header,body >.footer").hide();
         indexdom.on("click", ".iconfont", function() {
             indexdom.toggleClass("active");
         });
-
-
     };
-
 })
--- app/static/js/newslist.js
+++ app/static/js/newslist.js
@@ -1,4 +1,28 @@
 $(function() {
-    var myScroll = new IScroll($(".onelist")[0], { mouseWheel: true });
-    document.addEventListener('touchmove', function(e) { e.preventDefault(); }, false);
+
+
+
+    var vm = new Vue({
+        el: "#newslist",
+        data: {
+            list: [{
+                title: "这个的撒发生的标题",
+                desc: "着啊士大夫倒萨，描述啊不错的",
+                time: "03/04/2017"
+            }, {
+                title: "这个的撒发生的标题",
+                desc: "着啊士大夫倒萨，描述啊不错的着啊士大夫倒萨，描述啊不错的",
+                time: "03/04/2017"
+            }]
+        },
+        mounted: function() {
+            this.$nextTick(function() {
+                var myScroll = new IScroll($(".onelist")[0], { mouseWheel: true, scrollbars: false, interactiveScrollbars: true });
+                document.addEventListener('touchmove', function(e) { e.preventDefault(); }, false);
+            })
+        }
+
+
+
+    });
 })
