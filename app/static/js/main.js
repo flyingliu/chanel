230e21a6d86ca59254b3ee7e366a7e0abd9d436b
flying <fly0733@gmail.com>
2017-02-13 00:10:30 +0800

    a

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -2,9 +2,41 @@ $(function() {
     var fontSize = parseFloat(document.documentElement.style.fontSize);
     var indexdom = $(".indexmenu");
     if (indexdom.length > 0) {
+        var load = new Loading();
+
         $("body >.header,body >.footer").hide();
+        var bgimg = new Image();
+        bgimg.src = $(".bgimg")[0].src;
+        bgimg.onload = function() {
+            load.hide();
+        }
+        $("body").swipe({
+            //Generic swipe handler for all directions
+            swipe: function(event, direction, distance, duration, fingerCount, fingerData) {
+
+                if (direction == "down") {
+                    indexdom.removeClass("active");
+                } else if (direction == "up") {
+                    indexdom.addClass("active");
+                }
+            },
+            //Default is 75px, set to 0 for demo so any distance triggers swipe
+            threshold: 0
+        });
+
         indexdom.on("click", ".iconfont", function() {
             indexdom.toggleClass("active");
         });
     };
 })
+
+function Loading() {
+    $("body").append('<div class="loading"></div');
+}
+
+var loadingFn = Loading.prototype;
+loadingFn.hide = function() {
+    if ($(".loading").length > 0) {
+        $(".loading").remove();
+    }
+}
