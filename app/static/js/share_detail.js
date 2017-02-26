72fbb0b0415c6a44f25199a83a9822c88b38d4d8
flying <fly0733@gmail.com>
2017-02-26 10:26:31 +0800

    sa

--- app/static/js/share_detail.js
+++ app/static/js/share_detail.js
@@ -5,6 +5,7 @@ $(function() {
     var domain = location.port == "8080" ? "//devch.minuteschina.com/" : "";
     var getList = domain + '/share_getList.html';
     var addShare = domain + '/share_add.html';
+    var shareSend = domain + '/share_send.html';
     var tmpPage = 1;
     var myScroll;
     var pid = getQueryString("pid");
@@ -40,8 +41,30 @@ $(function() {
             },
             saveLocal: function() {
                 this.isShowForm = false;
-                localStorage.setItem("name", this.toUser.name);
-                localStorage.setItem("sex", this.toUser.sex);
+                if (this.toUser.name) {
+                    localStorage.setItem("name", this.toUser.name);
+                }
+                shareSendFn(2);
+
+
+            },
+            selectMale: function() {
+                var _this = this;
+                //询问框
+                layer.open({
+                    className: "layerbtn",
+                    content: '选择性别',
+                    btn: ['女士', '男士'],
+                    no: function(index) {
+                        localStorage.setItem("sex", 1);
+                        _this.toUser.sex = "男士";
+                    },
+                    yes: function(index) {
+                        localStorage.setItem("sex", 2);
+                        _this.toUser.sex = "女士";
+                        layer.close(index);
+                    }
+                });
             }
         },
         mounted: function() {
@@ -74,6 +97,27 @@ $(function() {
         })
     }
 
+    function shareSendFn(shareid) {
+        var name = localStorage.getItem("name") ? localStorage.getItem("name") : "未填写姓名";
+        var sex = localStorage.getItem("sex") == "1" ? "男士" : "女士";
+        $.ajax({
+            type: "POST",
+            url: shareSend,
+            data: {
+                shareid: shareid,
+                touser: name,
+                sex: sex
+            },
+            success: function(res) {
+                var data = JSON.parse(res);
+                if (data.status == 200) {
+                    msg(data.data);
+                } else {
+                    msg(data.data);
+                }
+            }
+        })
+    }
 
 })
 
@@ -86,4 +130,17 @@ $(function() {
  * 	401:参数缺失
  * 	402:授权错误
  * 	{"status":"200","data":数据}
+ */
+
+/**
+ * 接口名称：分享成功回调接口（微信js分享接口成功时调用）
+ * URL	/share_send.html
+ * 方式	POST
+ * 参数：shareid 分享列表id
+ * 	touser 分享对象姓名
+ * 	sex  1 男士 2女士
+ *  返回 200:成功
+ * 		401:参数缺失
+ * 		402:授权错误
+ * 		{"status":"200","data":"success"}
  */
